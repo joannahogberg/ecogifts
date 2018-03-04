12032a4ffd1c0b0e6ed8a81acd2e3311c0430db2
Joanna Högberg <joanna.hogberg@gmail.com>
2018-03-04 22:23:59 +0100

    images added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
-import { renderRandom } from "../../actions/actionCreators";
 import './header.css';
-// import SearchBar from './../SearchBar/SearchBar';
-// import ButtonGroup from "./../ButtonGroup/ButtonGroup";
 
 class Header extends Component {
 
@@ -12,25 +8,10 @@ class Header extends Component {
     return (
       <header>
           <h1>
-          <Link to="/" onClick={()=>this.props.renderRandom()} className="logo-link">ecoGifts</Link>
+          <Link to="/" className="logo-link">ecoGifts</Link>
           </h1>
-          {/* <Link to="/gift-form">Sök present</Link>
-          <SearchBar /> */}
       </header>
     );
   }
 }
-
-function mapStateToProps(state) {
-  return {
-      gifts: state.gifts
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    renderRandom: () => dispatch(renderRandom())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
