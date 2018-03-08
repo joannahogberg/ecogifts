eb513876e2564a038a27043f139d731c0cb3a080
Joanna Högberg <joanna.hogberg@gmail.com>
2018-03-08 14:07:43 +0100

    mixins added

--- src/reducers/favorites.js
+++ src/reducers/favorites.js
@@ -1,9 +1,8 @@
 import initialState from "./initialState";
 import * as types from "../actions//actionTypes";
-
+// Reducer for gift favorites added by user to localstorage
 export default function favorites(state = initialState.favorites, action) {
   let newState;
-  
   switch (action.type) {
     case types.ADD_GIFT_TO_LIST:
       const addGift = state.find(gift => gift.id === action.gift.id);
@@ -18,8 +17,8 @@ export default function favorites(state = initialState.favorites, action) {
       }
     case types.REMOVE_GIFT_FROM_LIST:
       return state.filter(i => i.id !== action.gift.id);
-    
-      case types.ADD_TO_LOCALSTORAGE:
+
+    case types.ADD_TO_LOCALSTORAGE:
       if (action.favorites) {
         newState = action.favorites;
       } else {
--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,10 +1,8 @@
 import * as types from "../actions/actionTypes";
-
+// Reducer to change filter for sorting state.gifts
 const visibilityFilter = (state = types.SHOW_ALL, action) => {
-
     switch (action.type) {
         case types.SET_VISIBILITY_FILTER:
-            console.log(action.filter)
             return action.filter
         default:
             return state
