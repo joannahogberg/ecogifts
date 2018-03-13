66e8e852835118640c4345479557ced50b22685e
cillami <cillaminami@gmail.com>
2018-03-13 10:12:04 +0100

    moved component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 // import { Link } from "react-router-dom"
 // import { connect } from "react-redux"
 
@@ -9,7 +8,7 @@ class About extends Component {
     return (
 
       <div>
-        hej
+        EcoGifts
       </div>
     )
   }
